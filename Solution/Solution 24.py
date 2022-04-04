c474536d261ebb2bd2fbffdd22f4aed0ceae2d32
Laxman sirvi <100264240+21BCA90@users.noreply.github.com>
2022-04-04 13:44:36 +0530

    Update Solution 24.py

--- Solution/Solution 24.py
+++ Solution/Solution 24.py
@@ -4,7 +4,7 @@
 def primeNo():
     n=int(input('Enter the value of N : '))
     print('Following are the prime No. between 1 to',n)
-    for i in range(1,n):
+    for i in range(1,n+1):
         if i>1:
             for j in range(2,i):
                 if i%j==0:
